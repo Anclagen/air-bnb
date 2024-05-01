a03cc718c86fd8977715f2a7855c7209c1605aa4
Anclagen <91462616+Anclagen@users.noreply.github.com>
2024-05-01 23:17:23 +0200

    nav bar updates and search modal added with placeholder js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import { Router } from "./components/router/router.js";
 import { Nav } from "./components/nav/nav.js";
+import { SearchModal } from "./components/search/searchModal.js";
 import { Theme } from "./components/theme/theme.js";
 
 $("document").ready(function () {
@@ -7,8 +8,8 @@ $("document").ready(function () {
     Router();
     Nav();
   }
-
   Navigation();
+  SearchModal();
   Theme();
   addEventListener("popstate", Navigation);
 });
---
+++ src/components/search/searchModal.js
@@ -0,0 +1,12 @@
+export async function SearchModal() {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const form = $(".searchModalForm");
+  form.on("submit", function (e) {
+    e.preventDefault();
+    const formData = form.serializeArray();
+    const search = formData[0].value;
+    console.log(search);
+    //clear search input
+    form.trigger("reset");
+  });
+}
