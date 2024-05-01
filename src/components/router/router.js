f9a9b27342f6e8eaaa0b927cf01242b458ed4e18
Anclagen <91462616+Anclagen@users.noreply.github.com>
2024-05-01 23:19:49 +0200

    router updated

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,31 +1,22 @@
 import { Home } from "../../pages/home/index.js";
 import { Profile } from "../../pages/profile/index.js";
 import { Map } from "../../pages/map/index.js";
-import { Auth } from "../../pages/auth/index.js";
-import { loadMap } from "../map/venueMap.js";
 
 export function Router() {
-  const root = $("#root-main");
   // Render page
   switch (window.location.hash) {
     case "":
-      root.html(Home());
+      Home();
       return;
     case "#profile":
-      root.html(Profile());
+      Profile();
       return;
     case "#map":
-      root.html(Map());
-      loadMap();
-      return;
-    case "#auth":
-    case "#auth?state=login":
-    case "#auth?state=register":
-      root.html(Auth());
+      Map();
       return;
     default:
       console.log("default");
-      root.html(Home());
+      Home();
       return;
   }
 }
