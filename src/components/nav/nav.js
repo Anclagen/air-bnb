0714e5923da4930d5d5ff7cf8fcc438c4e3dd11e
Anclagen <91462616+Anclagen@users.noreply.github.com>
2024-05-01 23:20:38 +0200

    active link and theme bug fixes

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,5 +1,5 @@
 export function Nav() {
-  const nav = $(".nav-link");
+  const nav = $(".nav-link, .dropdown-item");
   nav.removeClass("active");
   const hash = window.location.hash ? window.location.hash : "#";
   nav.filter(`[href="${hash}"]`).addClass("active");
