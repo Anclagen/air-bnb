7046bf5415b1de5f243f46a2412aec673cc0bc7f
Anclagen <91462616+Anclagen@users.noreply.github.com>
2024-05-03 20:10:28 +0200

    login and register api functions

---
+++ src/components/api/register.js
@@ -0,0 +1,20 @@
+import { apiBaseUrl } from "../../data/constants";
+
+export async function register(name, email, password) {
+  try {
+    const response = await $.ajax({
+      url: apiBaseUrl + "auth/register",
+      method: "POST",
+      data: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+      contentType: "application/json",
+    });
+    return response;
+  } catch (error) {
+    console.log("status:" + error.status, "message:" + error.responseJSON.message);
+    return { error: { status: error.status, message: error.responseJSON.message } };
+  }
+}
