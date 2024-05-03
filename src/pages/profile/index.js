d7335249ed65a80c116eee111ea1fa3380880b54
Anclagen <91462616+Anclagen@users.noreply.github.com>
2024-05-03 20:11:20 +0200

    redirect to home for session required pages

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,6 +1,19 @@
 export function Profile() {
+  console.log($("body[data-session=false]"));
+  if ($("body[data-session=false]").length > 0) {
+    // go to home page if false, using hash route
+    $("document").ready(function () {
+      window.location.hash = "#";
+    });
+    return;
+  }
+
+  const root = $("#root-main");
   const content = `<div>
       <h1>Profile</h1>
+      <button data-bs-toggle="modal" data-bs-target="#profileUpdateModal">Update Profile</button>
+      <button data-bs-toggle="modal" data-bs-target="#registerModal">Register</button>
     </div>`;
-  return content;
+
+  root.html(content);
 }
