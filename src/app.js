7f1fa7c23acb81000aeb8a61fa06bf55ad07cbb9
Anclagen <91462616+Anclagen@users.noreply.github.com>
2024-05-03 20:10:03 +0200

    error catcher for whole app

--- src/app.js
+++ src/app.js
@@ -3,13 +3,18 @@ import { Nav } from "./components/nav/nav.js";
 import { SearchModal } from "./components/search/searchModal.js";
 import { Theme } from "./components/theme/theme.js";
 
-$("document").ready(function () {
-  function Navigation() {
-    Router();
-    Nav();
-  }
-  Navigation();
-  SearchModal();
-  Theme();
-  addEventListener("popstate", Navigation);
-});
+try {
+  $("document").ready(function () {
+    function Navigation() {
+      Router();
+      Nav();
+    }
+    Navigation();
+    SearchModal();
+    Theme();
+    addEventListener("popstate", Navigation);
+  });
+} catch (error) {
+  console.error(error);
+  alert("An error occurred, please try again later.");
+}
