1f4fa4b91e8050a3d67df235cc89f0daa66caf07
Anclagen <91462616+Anclagen@users.noreply.github.com>
2024-05-09 15:19:40 +0200

    fixing options

--- src/components/api/getVenues.js
+++ src/components/api/getVenues.js
@@ -6,21 +6,22 @@ import { apiBaseUrl } from "../../data/constants.js";
  * @param {String} id - venue uuid
  * @returns
  */
-export async function getVenues(options = { sort: null, order: null, bookings: true, owner: true, limit: 100, offset: 0 }, id = null) {
+export async function getVenues(options = { sort: "created", order: "desc", bookings: true, owner: true, limit: 100, offset: 0 }, id = null) {
   try {
+    console.log(options);
     const response = await $.ajax({
       url:
         apiBaseUrl +
         "holidaze/venues" +
         (id ? `/${id}` : "") +
-        `?_limit=${options.limit}` +
-        (options.sort && !id ? `?_sort=${options.sort}` : "") +
-        (options.order && !id ? `&_order=${options.order}` : "") +
+        `?limit=${options.limit ? options.limit : 100}` +
+        `&offset=${options.offset ? options.offset : 0}` +
+        (options.sort && !id ? `&sort=${options.sort}` : "") +
+        (options.order && !id ? `&order=${options.order}` : "") +
         (options.bookings ? `&_bookings=${options.bookings}` : "") +
-        (options.owner ? `&_owner=${options.owner}` : "") +
-        `&_offset=${options.offset}`,
+        (options.owner ? `&_owner=${options.owner}` : ""),
     });
-
+    console.log(response);
     //filter out if "string" or "test" is in either the venue name and description
     const data = response.data.filter((venue) => {
       return !venue.name.toLowerCase().includes("string") && !venue.name.toLowerCase().includes("test") && !venue.description.toLowerCase().includes("test");
