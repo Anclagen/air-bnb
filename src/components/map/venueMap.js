82f440dbedab8a1507f4511a93f245471b215b75
Anclagen <91462616+Anclagen@users.noreply.github.com>
2024-07-28 14:05:47 +0200

    fix map bounds prevented world duplication and wrap

--- src/components/map/venueMap.js
+++ src/components/map/venueMap.js
@@ -1,18 +1,25 @@
 import { updateVenueModal } from "../templates/modals/venueModal.js";
 import L from "leaflet";
 import $ from "jquery";
+import { wifi, noWifi, parking, noParking, food, noFood, pet, noPet } from "../../icons/index.js";
+import { min } from "date-fns";
 
 export async function loadMap(data = []) {
+  const southWest = L.latLng(-85, -180);
+  const northEast = L.latLng(85, 180);
+  const bounds = L.latLngBounds(southWest, northEast);
   //set zoom on europe
-
   const osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     minZoom: 3, // Minimum zoom level
-    maxZoom: 19, // Maximum zoom level
+    maxZoom: 5, // Maximum zoom level
+    noWrap: true, // No wrapping of the map
   });
 
   const osmHOT = L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-    maxZoom: 19,
+    minZoom: 3,
+    maxZoom: 5,
     attribution: "© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France",
+    noWrap: true,
   });
 
   var baseMaps = {
@@ -20,9 +27,23 @@ export async function loadMap(data = []) {
     "OpenStreetMap.HOT": osmHOT,
   };
 
-  let map = L.map("venueMap", { zoomSnap: 0.25, center: [39.73, -104.99], zoom: 10, layers: [osm, osmHOT] });
+  let map = L.map("venueMap", { zoomSnap: 0.25, center: [50.0, 13.99], minZoom: 3, maxZoom: 5, maxBounds: bounds, maxBoundsViscosity: 1.0, layers: [osm, osmHOT], worldCopyJump: false });
+
+  // Display the current zoom level
+  const zoomLevelElement = document.getElementById("zoom-level");
+
+  function updateZoomLevel() {
+    zoomLevelElement.innerHTML = `Zoom Level: ${map.getZoom()}`;
+  }
+
+  map.setView([50.0, 13.99], 3);
+
+  // Update zoom level display initially and on zoom end
+  map.on("zoomend", updateZoomLevel);
+  updateZoomLevel();
 
   var layerControl = L.control.layers(baseMaps).addTo(map);
+  let markersGroup = L.layerGroup().addTo(map);
 
   data.forEach((venue, i) => {
     let lat = venue.location.lat;
@@ -40,17 +61,6 @@ export async function loadMap(data = []) {
         lng = Math.random() * (-66 - -125) - 125; // Longitude between -125 and -66
       }
     }
-    //   <div class="card m-2 w-100">
-    //   ${venue.media[0] ? `<img src="${venue.media[0].url}" class="card-img-top" alt="${venue.name}" />` : ""}
-    //   <div class="card-body">
-    //     <h5 class="card-title">${venue.name}</h5>
-    //     <p class="card-text">${venue.description}</p>
-    //     <p class="card-text">Max Guests: ${venue.maxGuests}</p>
-    //     <p class="card-text">Location: ${venue.location.city}, ${venue.location.country}</p>
-    //     <p> ${venue.meta.wifi ? wifi : noWifi} ${venue.meta.parking ? parking : noParking} ${venue.meta.breakfast ? food : noFood} ${venue.meta.pets ? pet : noPet}</p>
-    //     <button class="btn btn-primary p-2 px-3 ms-2" id="${venue.id}" data-bs-toggle="modal" data-bs-target="#venueModal">view</button>
-    //   </div>
-    // </div>
     if (lat || lng) {
       var popupContent = `<div class="card w-100 marker-card">
       ${venue.media[0] ? `<img src="${venue.media[0].url}" class="card-img-top-marker" alt="${venue.name}" />` : ""}
@@ -58,11 +68,13 @@ export async function loadMap(data = []) {
         <h5 class="card-title">${venue.name}</h5>
         <p class="card-text">${venue.description}</p>
         <p class="card-text">Max Guests: ${venue.maxGuests}</p>
+        <p> ${venue.meta.wifi ? wifi : noWifi} ${venue.meta.parking ? parking : noParking} ${venue.meta.breakfast ? food : noFood} ${venue.meta.pets ? pet : noPet}</p>
         <button class="btn btn-primary p-2 px-3 ms-2" id="${venue.id}" data-bs-toggle="modal" data-bs-target="#venueModal">view</button>
       </div>
     </div>`;
 
-      const marker = L.marker([lat, lng]).addTo(map).bindPopup(popupContent);
+      const marker = L.marker([lat, lng]).bindPopup(popupContent);
+      markersGroup.addLayer(marker);
 
       marker.on("popupopen", function () {
         $(`#${venue.id}`).on("click", () => {
@@ -71,41 +83,88 @@ export async function loadMap(data = []) {
       });
     }
   });
+
+  function filterMarkers(data, L, markersGroup) {
+    let options = {
+      wifi: document.getElementById("wifi").checked,
+      parking: document.getElementById("parking").checked,
+      breakfast: document.getElementById("breakfast").checked,
+      pets: document.getElementById("pets").checked,
+      guests: document.getElementById("guests").value,
+      guestsStrict: document.getElementById("guests-strict").checked, //exact match guest numbers
+      maxGuestOverFlow: document.getElementById("max-guest-overflow").value, // number over the guest number
+      minPrice: document.getElementById("min-price").value,
+      maxPrice: document.getElementById("max-price").value,
+      minRating: document.getElementById("min-rating").value,
+    };
+
+    let filteredData = data.filter((venue) => {
+      if (options.wifi && !venue.meta.wifi) {
+        return false;
+      }
+      if (options.parking && !venue.meta.parking) {
+        return false;
+      }
+      if (options.breakfast && !venue.meta.breakfast) {
+        return false;
+      }
+      if (options.pets && !venue.meta.pets) {
+        return false;
+      }
+      if (options.guests && venue.maxGuests < options.guests) {
+        return false;
+      }
+      if (options.guestsStrict && venue.maxGuests !== options.guests) {
+        return false;
+      }
+      if (options.maxGuestOverFlow && venue.maxGuests < options.guests + options.maxGuestOverFlow) {
+        return false;
+      }
+      if (options.minPrice && venue.price < options.minPrice) {
+        return false;
+      }
+      if (options.maxPrice && venue.price > options.maxPrice) {
+        return false;
+      }
+      if (options.minRating && venue.rating < options.minRating) {
+        return false;
+      }
+      return true;
+    });
+  }
 }
 
-// import { config } from "dotenv";
-// config();
-
-// export async function loadMap(data = []) {
-//   const response = await fetch("https://v2.api.noroff.dev/holidaze/venues");
-//   const response = await fetch("https://api.noroff.dev/api/v1/holidaze/venues?limit=1");
-//   const data = await response.json();
-//   console.log(data);
-//   Address conversion to coordinates using geocode.xyz, but bulk processing on free tier is limited to what fits in a single URL. This is a problem when there are many venues.
-//   const addresses = data.map(
-//     (venue) =>
-//       `${venue.location.address ? venue.location.address + ", " : ""} ${venue.location.postalCode ? venue.location.postalCode + ", " : ""}, ${venue.location.city ? venue.location.city + ", " : ""}, ${
-//         venue.location.country ? venue.location.country : ""
-//       }`
-//   );
-//   const bulkProcessing = await fetch(`https://geocode.xyz/${addresses.join("|")}?json=1&auth=${process.env.GEOCODE_API_KEY}`);
-//   const coords = await response.json();
-//   console.log(bulkProcessing);
-
-//   var mymap = L.map("venueMap").setView([10, 10], 10);
-
-//   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//     maxZoom: 19,
-//   }).addTo(mymap);
-
-//   data.forEach((venue, i) => {
-//     if (venue.location.lat && venue.location.lng) {
-//       var popupContent = `<b>${venue.name}</b><br />${venue.description}`;
-
-//       if (venue.media && venue.media.length > 0) {
-//         popupContent += `<br /><img src="${venue.media[0].url}" alt="${venue.media[0].alt}" style="width: 100px;">`;
-//       }
-//       L.marker([venue.location.lat, venue.location.lng]).addTo(mymap).bindPopup(popupContent).openPopup();
-//     }
-//   });
-// }
+const exampleData = {
+  id: "9f228eb9-c200-4ac4-ac98-2a1e8544bd55",
+  name: "zzzGreat Venue",
+  description: "this is an awesome venue",
+  media: [
+    {
+      url: "https://idologyasheville.com/img/Things-You-Never-Knew-About-Your-Lakehouse-Home-Floors_IDology-Asheville.jpg",
+      alt: "My media",
+    },
+  ],
+  price: 3000,
+  maxGuests: 4,
+  rating: 0,
+  created: "2024-05-19T17:28:36.644Z",
+  updated: "2024-05-28T20:16:30.012Z",
+  meta: {
+    wifi: false,
+    parking: false,
+    breakfast: true,
+    pets: false,
+  },
+  location: {
+    address: "Oslo",
+    city: "Oslo",
+    zip: null,
+    country: "Norway",
+    continent: null,
+    lat: 0,
+    lng: 0,
+  },
+  _count: {
+    bookings: 93,
+  },
+};
--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,15 +1,23 @@
 import { loadMap } from "../../components/map/venueMap.js";
 import { apiBaseUrl } from "../../data/constants.js";
 import $ from "jquery";
+import L from "leaflet";
 
 export function Map() {
   const root = $("#root-main");
   let content = `<section>
       <h1>Map</h1>
+        <div id="zoom-level">Zoom Level: </div>
+      <div class="position-relative">
+      <div class="position-absolute top-0 left-0 z-1 p-2">
+        <button class="btn btn-primary" id="back">Back</button>
+      </div>
       <div class="z-2" id="venueMap" style="height: 100vh;"></div>
+      <div>
     </section>`;
   root.html(content);
-  // fetch data
+
+  //fetch data
   $.ajax({
     url: apiBaseUrl + "holidaze/venues",
     success: function (response) {
